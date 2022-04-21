6d7e01f109fab4d1c3ad1f8539b2c7c2f03aed3b
alberto <alberto.harres@gmail.com>
2022-04-22 00:29:44 +0200

    display characters

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
 const socket = io.connect('http://localhost:3000');
 
-
 const onTextSound = function (textSound) {
   console.log("textSound", textSound)
   text2Audio(textSound)
+  const textEl = document.getElementById("text")
+  textEl.innerText=textSound
 }
 
 socket.on("textSound", onTextSound);
+
